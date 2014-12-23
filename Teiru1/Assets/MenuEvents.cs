a1e24b3cb60b363d02cab032393cf31297b6cd85
Monk <kolapser@gmail.com>
2014-12-23 15:04:53 +0200

    0.1a

---
+++ Teiru1/Assets/MenuEvents.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuEvents : MonoBehaviour 
+{
+	public void LoadLevel(int levelIndex)
+	{
+		Application.LoadLevel(levelIndex);
+	}
+	
+	public void LoadLevel(string levelName)
+	{
+		Application.LoadLevel(levelName);
+	}
+	
+	public void ExitApplication()
+	{
+		Application.Quit();
+	}
+
+}
