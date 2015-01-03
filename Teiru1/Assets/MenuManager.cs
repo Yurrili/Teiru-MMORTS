8959161061eedd82bfecc0a89efbf7d102df1fd1
Monk <kolapser@gmail.com>
2015-01-03 20:42:23 +0100

    buttons

--- Teiru1/Assets/MenuManager.cs
+++ Teiru1/Assets/MenuManager.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class MenuManager : MonoBehaviour {
 
 	public Menu CurrentMenu;
-
+	GameObject obj;
+	GameObject obj1;
 	public void Start()
 	{
 		ShowMenu (CurrentMenu);
+		obj = GameObject.FindGameObjectWithTag ("Back");
+		obj.SetActive (false);
 	}
 
 	public void ShowMenu(Menu menu)
@@ -19,4 +22,34 @@ public class MenuManager : MonoBehaviour {
 		CurrentMenu.isOpen = true;
 	}
 
+	public void OnMouseDown()
+	{
+		if (obj)
+			obj.SetActive (false);
+		
+	}
+
+	public void OnMouseDown1()
+	{
+		if (obj)
+			obj.SetActive (true);
+		
+	}
+
+	public void OnMouseDownLog()
+	{
+		obj1 = GameObject.FindGameObjectWithTag ("Registration");
+		if (obj1)
+			obj1.SetActive (false);
+		
+	}
+	
+	public void OnMouseDownLOg1()
+	{
+		if (obj1)
+			obj1.SetActive (true);
+		
+	}
+
+	
 }
