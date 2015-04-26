93914c0b489582d19ae70369fde749fb41dda42c
Yurrili <yurrili931@gmail.com>
2015-04-26 23:28:20 +0200

    dfsf
    sdfsdfsfs

--- Teiru1/Assets/OnHoverEventsBitch.cs
+++ Teiru1/Assets/OnHoverEventsBitch.cs
@@ -2,8 +2,11 @@
 using System.Collections;
 using UnityEngine.UI;
 
+
 public class OnHoverEventsBitch : MonoBehaviour {
 
+
+	CharacterClass classCha;
 	Text text;
 	// Use this for initialization
 	void Start () 
@@ -11,24 +14,61 @@ public class OnHoverEventsBitch : MonoBehaviour {
 
 		text = GameObject.Find ("AboutSkill").GetComponentInChildren<Text>();
 	}
+
+	private void classchoose(string a){
+
+		switch(a)
+		{
+		case "Ranger":
+			classCha = new CharacterClass_Ranger(0);
+
+			print ("Ranger");
+			break;
+		case "Mage":
+			classCha = new CharacterClass_Mage(0);
+
+			print ("Mage");
+			break;
+		case "Knight":
+			classCha = new CharacterClass_Knight(0);
+
+			print ("Knight");
+			break;
+		case "Mystic":
+			classCha = new CharacterClass_Mystiq(0);
+
+			print ("Mystic");
+			break;
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		classchoose(GameObject.Find ("ClassTextPlace").GetComponent<Text>().text);
 	}
 
 	public void onToggle11IsEntered()
 	{
-		text.text = "OLOLO";
+		text.text = classCha.getSkillDescription (0, 0);
 	}
 
 	public void onToggle12IsEntered()
 	{
-		text.text = "OLOLO2222";
+		text.text = classCha.getSkillDescription (0, 1);
 	}
 
 	public void onToggle13IsEntered()
 	{
-		text.text = "Gustaw Ober Folksdojtcz";
+		text.text = classCha.getSkillDescription (0, 2);
+	}
+
+	public void onToggle14IsEntered()
+	{
+		text.text = classCha.getSkillDescription (0, 3);
+	}
+
+	public void onToggle15IsEntered()
+	{
+		text.text = classCha.getSkillDescription (0, 4);
 	}
 }
