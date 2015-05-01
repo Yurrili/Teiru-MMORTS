6d47c9bce21e586a1fe2e69a9b50449b018bc1fe
Yurrili <yurrili931@gmail.com>
2015-05-01 20:53:09 +0200

    Equ- almost DONE

---
+++ Teiru1/Assets/Boots.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boots : Item {
+
+
+	public Boots(int a)
+	{
+		base.itemDD = a;
+		if (a == 0) {
+			base.name = "Boots of the Celestial Battalion";
+			base.Description = "This boots are so fine and light that they can fly \n " ;
+			base.cost = 20;
+			base.isDestroyed = false;
+			base.Part = "Boots";
+			base.Type = "Light boots";
+			CharacterStats temp = new CharacterStats ();
+			temp.setCHA (0);
+			temp.setCON (0);
+			temp.setINT (0);
+			temp.setDEX (2);
+			temp.setSTR (0);
+			temp.setWIS (0);
+			base.bonus = temp;
+		} else {
+			if (a == 1) {
+				base.name = "Elven Boots";
+				base.Description = "This extremely light boots are made of very fine mithral aglets. \n " +
+					"It has a maximum Dexterity bonus of +3 and Inteligent bonus +2 .";
+				base.cost = 40;
+				base.isDestroyed = false;
+				base.Part = "Boots";
+				base.Type = "Light boots";
+				CharacterStats temp = new CharacterStats ();
+				temp.setCHA (0);
+				temp.setCON (0);
+				temp.setINT (2);
+				temp.setDEX (3);
+				temp.setSTR (0);
+				temp.setWIS (0);
+				base.bonus = temp;
+			} else {
+				if (a == 2) {
+					base.name = "Warlord’s Boots";
+					base.Description = "This mithral aglets in boots allows the wearer \nto attract and lead a number of followers ";
+					
+					base.cost = 60;
+					base.isDestroyed = false;
+					base.Part = "Boots";
+					base.Type = "Light boots";
+					CharacterStats temp = new CharacterStats ();
+					temp.setCHA (2);
+					temp.setCON (0);
+					temp.setINT (2);
+					temp.setDEX (5);
+					temp.setSTR (0);
+					temp.setWIS (0);
+					base.bonus = temp;
+				} else {
+					if (a == 3) {
+						base.name = "Demon Boots";
+						base.Description = "This armor boots is fashioned to make the wearer appear \n" + 
+							"to be a demon. The pictures on the heals are shaped to look like a horned demon head";
+						base.cost = 20;
+						base.isDestroyed = false;
+						base.Part = "Boots";
+						base.Type = "Heavy boots";
+						CharacterStats temp = new CharacterStats ();
+						temp.setCHA (0);
+						temp.setCON (1);
+						temp.setINT (0);
+						temp.setDEX (0);
+						temp.setSTR (2);
+						temp.setWIS (0);
+						base.bonus = temp;
+					} else {
+						if (a == 5) {
+							base.name = "Dragonskin Boots";
+							base.Description = "This boots are crafted from the hide of a great wyrm dragon.\n" +
+								" At the wearer’s command, the boots sprouts enormous dragon wings, \n" +
+									"allowing the wearer to fly .";
+							base.cost = 60;
+							base.isDestroyed = false;
+							base.Part = "Boots";
+							base.Type = "Heavy boots";
+							CharacterStats temp = new CharacterStats ();
+							temp.setCHA (1);
+							temp.setCON (2);
+							temp.setINT (0);
+							temp.setDEX (4);
+							temp.setSTR (0);
+							temp.setWIS (0);
+							base.bonus = temp;
+						} else {
+							if (a == 4) {
+								base.name = "Boots of Luck";
+								base.Description = "Ten 100-gp gems adorn this boots. \n " + "Gives wearer a bonus + 3 to strength and +1 to condition ";
+								base.cost = 40;
+								base.isDestroyed = false;
+								base.Part = "Boots";
+								base.Type = "Heavy boots";
+								CharacterStats temp = new CharacterStats ();
+								temp.setCHA (1);
+								temp.setCON (1);
+								temp.setINT (0);
+								temp.setDEX (2);
+								temp.setSTR (0);
+								temp.setWIS (0);
+								base.bonus = temp;
+							}
+							
+						}
+					}
+				}
+			}
+		}}
+} 
+
---
+++ Teiru1/Assets/OnHoverEventsItem.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class OnHoverEventsItem : MonoBehaviour {
+
+
+	public Text AboutItem;
+	Text Desctext;
+
+	void Start () 
+	{
+
+		AboutItem = GameObject.Find ("AboutItem").GetComponentInChildren<Text>();
+	
+	}
+
+	//Chest
+
+	public void onToggleChest1()
+	{
+		AboutItem.text = (new Chest (0)).getAllDescription();
+	}
+
+	public void onToggleChest2()
+	{
+		AboutItem.text = (new Chest (1)).getAllDescription();
+	}
+
+	public void onToggleChest3()
+	{
+		AboutItem.text = (new Chest (2)).getAllDescription();
+	}
+
+	public void onToggleChest4()
+	{
+		AboutItem.text = (new Chest (3)).getAllDescription();
+	}
+
+	public void onToggleChest5()
+	{
+		AboutItem.text = (new Chest (4)).getAllDescription();
+	}
+
+	
+	public void onToggleChest6()
+	{
+		AboutItem.text = (new Chest (5)).getAllDescription();
+	}
+
+	//Helmet
+
+	public void onToggleHelmet1()
+	{
+		AboutItem.text = (new Helmet (0)).getAllDescription();
+	}
+	
+	public void onToggleHelmet2()
+	{
+		AboutItem.text = (new Helmet (1)).getAllDescription();
+	}
+	
+	public void onToggleHelmet3()
+	{
+		AboutItem.text = (new Helmet (2)).getAllDescription();
+	}
+	
+	public void onToggleHelmet4()
+	{
+		AboutItem.text = (new Helmet (3)).getAllDescription();
+	}
+	
+	public void onToggleHelmet5()
+	{
+		AboutItem.text = (new Helmet (4)).getAllDescription();
+	}
+	
+	
+	public void onToggleHelmet6()
+	{
+		AboutItem.text = (new Helmet (5)).getAllDescription();
+	}
+
+
+	//Boots
+
+	
+	public void onToggleBoots1()
+	{
+		AboutItem.text = (new Boots (0)).getAllDescription();
+	}
+	
+	public void onToggleBoots2()
+	{
+		AboutItem.text = (new Boots (1)).getAllDescription();
+	}
+	
+	public void onToggleBoots3()
+	{
+		AboutItem.text = (new Boots (2)).getAllDescription();
+	}
+	
+	public void onToggleBoots4()
+	{
+		AboutItem.text = (new Boots (3)).getAllDescription();
+	}
+	
+	public void onToggleBoots5()
+	{
+		AboutItem.text = (new Boots (4)).getAllDescription();
+	}
+	
+	
+	public void onToggleBoots6()
+	{
+		AboutItem.text = (new Boots (5)).getAllDescription();
+	}
+
+	//weapon
+
+	//Helmet
+	
+	public void onToggleWeapon1()
+	{
+		AboutItem.text = (new Weapon (0)).getAllDescription();
+	}
+	
+	public void onToggleWeapon2()
+	{
+		AboutItem.text = (new Weapon (1)).getAllDescription();
+	}
+	
+	public void onToggleWeapon3()
+	{
+		AboutItem.text = (new Weapon (2)).getAllDescription();
+	}
+	
+	public void onToggleWeapon4()
+	{
+		AboutItem.text = (new Weapon (3)).getAllDescription();
+	}
+	
+	public void onToggleWeapon5()
+	{
+		AboutItem.text = (new Weapon (4)).getAllDescription();
+	}
+	
+	
+	public void onToggleWeapon6()
+	{
+		AboutItem.text = (new Weapon (5)).getAllDescription();
+	}
+
+}
