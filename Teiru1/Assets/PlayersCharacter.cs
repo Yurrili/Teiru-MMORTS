237b10ce0a8683d47192cb5b2a6fc8101c85ab82
Yurrili <yurrili931@gmail.com>
2015-05-06 15:14:10 +0200

    Viewer of character

---
+++ Teiru1/Assets/PlayersCharacter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayersCharacter {
+
+	private string PlayerName;
+	private string Name;
+	private CharacterClass Class_;
+	private CharacterStats Statistics;
+
+
+	public PlayersCharacter(string PlayerName, string CharacterName,int ClassOfCharacter, int STR, int DEX, int CON, int INT , int WIS, int CHA ) {
+
+		this.PlayerName = PlayerName;
+		this.Name = CharacterName;
+		this.Statistics = new CharacterStats ();
+
+			Statistics.setCHA (CHA);
+			Statistics.setCON (CON);
+			Statistics.setINT (INT);
+			Statistics.setWIS (WIS);
+			Statistics.setDEX (DEX);
+			Statistics.setSTR (STR);
+
+		switch( ClassOfCharacter )
+		{
+			case 0 :
+				Class_ = new CharacterClass_Ranger(CON);
+			break;
+
+			case 1 :
+				Class_ = new CharacterClass_Mage(CON);
+			break;
+
+			case 2 :
+				Class_ = new CharacterClass_Knight(CON);
+			break;
+
+			case 3 :
+				Class_ = new CharacterClass_Mystiq(CON);
+			break;
+		}
+
+
+	}
+}
--- Teiru1/Assets/ShowACharacter.cs
+++ Teiru1/Assets/ShowACharacter.cs
@@ -1,15 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ShowACharacter : MonoBehaviour {
 
+	public Text SpellButton1;
+	public Text SpellButton2;
+	public Text SpellButton3;
+	public Text SpellButton4;
+	public Text SpellButton5;
+
+	public GameObject Rank1But;
+	public GameObject Rank2But;
+	public GameObject Rank3But;
+	public GameObject Rank4But;
+	public GameObject Rank5But;
+
+	private GameObject[] RanksButtons;
+
+	private CharacterClass ActiveCharacter = new CharacterClass_Ranger(8);
+
 	// Use this for initialization
 	void Start () {
-	
+		SpellButton1.text = "";
+		SpellButton2.text = "a";
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+
+	public void setData(){
+		GameObject.Find ("STRStats").GetComponentInChildren<Text>() = ActiveCharacter;
+		GameObject.Find ("DEXStats").GetComponentInChildren<Text>();
+		GameObject.Find ("CONStats").GetComponentInChildren<Text>();
+		GameObject.Find ("INTStats").GetComponentInChildren<Text>();
+		GameObject.Find ("WISStats").GetComponentInChildren<Text>();
+		GameObject.Find ("CHAStats").GetComponentInChildren<Text>();
+	}
+
+	private void setDesactivatedRanksButton(int a){
+
+	}
+
+	public void ShowonRankButtons(){
+		
+		Rank2But.SetActive (false);
+		Rank3But.SetActive (false);
+		Rank4But.SetActive (false);
+		Rank5But.SetActive (false);
+
+	}
+
+
 }
